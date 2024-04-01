82eb8c7a1ddaee8d137ed3f9b3dbe5032a5d3404
Daiana de Paula <daianaadepaula1@gmail.com>
2024-04-01 09:11:27 -0300

    feat: skeleton section component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
   };
 
   return (
-    <div
+    <header
       className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
@@ -86,7 +86,7 @@ const Header = () => {
           <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
-    </div>
+    </header>
   );
 };
 
